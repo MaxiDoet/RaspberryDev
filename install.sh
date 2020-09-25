adfc20edb77ad940c2d088a3bf4000691432e283
TheHeroCraft1579 <maximilian.doetsch@outlook.de>
2020-09-25 16:41:58 +0100

    Update

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 echo "Updating Raspian"
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt update
+sudo apt upgrade
 
 echo "Installing Desktop"
-sudo apt-get install xorg lightdm xfce4
+sudo apt install xorg lightdm xfce4
 
 echo "Installing Dev Essentials"
-sudo apt-get install git npm
+sudo apt install git npm
 echo "Installing NodeJS"
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 sudo nvm install 14.11.0
 echo "Installing Desktop Tools (Terminal Emulator,...)"
-sudo apt-get install konsole
-sudo apt-get install plank
-sudo apt-get install chromium
+sudo apt install konsole
+sudo apt install plank
+sudo apt install chromium
 
 echo "Installing Themes"
 cp -r Dracula /usr/share/themes/
